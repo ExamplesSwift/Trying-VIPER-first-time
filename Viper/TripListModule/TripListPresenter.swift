9ed051a7421e6a7526fe63af44447abeb643c4ed
Henry AT <yiperu@gmail.com>
2020-06-06 12:00:55 -0400

    Add a new variable to the class - you want to expose the list of trips from the data model

--- Viper/TripListModule/TripListPresenter.swift
+++ Viper/TripListModule/TripListPresenter.swift
@@ -4,6 +4,8 @@ import Combine
 class TripListPresenter: ObservableObject {
   private let interactor: TripListInteractor
   
+  @Published var trips: [Trip] = []
+  
   init(interactor: TripListInteractor) {
     self.interactor = interactor
   }

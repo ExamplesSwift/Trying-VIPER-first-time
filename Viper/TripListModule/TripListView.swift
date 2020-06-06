f7344c6f77f00f62c058775c28cff85c70c1b918
Henry AT <yiperu@gmail.com>
2020-06-06 17:10:45 -0400

    Add the following property to TripListView:

--- Viper/TripListModule/TripListView.swift
+++ Viper/TripListModule/TripListView.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 struct TripListView: View {
-
+  @ObservedObject var presenter: TripListPresenter
     
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)

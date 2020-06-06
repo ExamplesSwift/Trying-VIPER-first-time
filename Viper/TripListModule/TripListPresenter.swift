2f303c9993092430c38e7311f44ab4a438cc9fe6
Henry AT <yiperu@gmail.com>
2020-06-06 12:02:54 -0400

    The next step is to synchronize this list with the data model from the interactor. First, add the following helper property

--- Viper/TripListModule/TripListPresenter.swift
+++ Viper/TripListModule/TripListPresenter.swift
@@ -4,6 +4,8 @@ import Combine
 class TripListPresenter: ObservableObject {
   private let interactor: TripListInteractor
   
+  private var cancellables = Set<AnyCancellable>()
+  
   @Published var trips: [Trip] = []
   
   init(interactor: TripListInteractor) {

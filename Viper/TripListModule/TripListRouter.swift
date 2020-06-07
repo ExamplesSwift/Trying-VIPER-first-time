487b4f9346bc372986ae8ab52145b34df38ae6ed
Henry AT <yiperu@gmail.com>
2020-06-06 21:14:44 -0400

    Set the content of Roting file This class outputs a new TripDetailView that’s been populated with an interactor and presenter. The router handles transitioning from one screen to another, setting up the classes needed for the next view.

--- Viper/TripListModule/TripListRouter.swift
+++ Viper/TripListModule/TripListRouter.swift
@@ -1,1 +1,13 @@
-import Foundation
+import SwiftUI
+
+class TripListRouter {
+  func makeDetailView(for trip: Trip, model: DataModel) -> some View {
+    let presenter = TripDetailPresenter(interactor:
+      TripDetailInteractor(trip:
+        trip, model:
+        model, mapInfoProvider:
+        RealMapDataProvider()))
+    return TripDetailView(presenter: presenter)
+    
+  }
+}

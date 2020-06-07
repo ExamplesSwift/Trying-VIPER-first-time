f47fa48cf284c2b734f601213ddeb6fbdf0f0df8
Henry AT <yiperu@gmail.com>
2020-06-06 20:46:40 -0400

    Set content of Presenter, in TripDetailPresenter, set its contents to

--- Viper/TripDetailModule/TripDetailPresenter.swift
+++ Viper/TripDetailModule/TripDetailPresenter.swift
@@ -1,1 +1,13 @@
-import Foundation
+import Combine
+import SwiftUI
+
+class TripDetailPresenter: ObservableObject {
+  private let interactor: TripDetailInteractor
+  
+  private var cancellable = Set<AnyCancellable>()
+  
+  init(interactor: TripDetailInteractor) {
+    self.interactor = interactor
+  }
+  
+}

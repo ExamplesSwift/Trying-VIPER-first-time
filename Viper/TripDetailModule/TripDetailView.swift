0dfa16b9e090c27952509952c64cb43d35843050
Henry AT <yiperu@gmail.com>
2020-06-06 20:59:07 -0400

    Create  previews building again, change that stub to: Make the preview, Now the view will build, but the preview is still just “Hello, World!”

--- Viper/TripDetailModule/TripDetailView.swift
+++ Viper/TripDetailModule/TripDetailView.swift
@@ -1,6 +1,8 @@
 import SwiftUI
 
 struct TripDetailView: View {
+  @ObservedObject var presenter: TripDetailPresenter
+  
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
@@ -8,6 +10,13 @@ struct TripDetailView: View {
 
 struct TripDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        TripDetailView()
+      let model = DataModel.sample
+      let trip = model.trips[1]
+      let mapProvider = RealMapDataProvider()
+      let presenter = TripDetailPresenter(interactor: TripDetailInteractor(trip: trip, model: model, mapInfoProvider: mapProvider))
+      
+      return NavigationView {
+        TripDetailView(presenter: presenter)
+      }
     }
 }
